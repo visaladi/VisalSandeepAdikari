4d333e7bbe72cb83837694fbed3c24579e0a2aa1
visal sandeep adikari <122593452+visaladi@users.noreply.github.com>
2024-02-26 14:46:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let type = new Typed('#changing-text',{
+    strings: ['Full Stack Developer','Web Developer','UI/UX Designer', 'Front-end Developer', 'Youtuber', 'Flutter Expert'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+})
